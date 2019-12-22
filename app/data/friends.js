727bb27f0a5059305aaae5103b9e8c495bebffd9
J. Silverthorn <jnsilverthorn@ucdavis.edu>
2019-12-21 20:32:42 -0800

    did a decent amount, but dont have the survey functionality yet

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+    {
+        name: "Lucy",
+        img: "placeholder",
+        score: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            6,
+            6,
+            6,
+            6
+        ]
+    }
+];
+
+module.exports = friends;
